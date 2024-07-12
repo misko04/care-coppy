78a58a2aadb2a4ddb35c153a45b83aaf60541e2f
Michal Zachardala <misozach@gmail.com>
2024-07-12 16:20:01 +0200

    Update doctors-cards.vue

--- components/doctors-cards.vue
+++ components/doctors-cards.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="flex gap-[16px] md:flex-col md:items-center xl:gap-[24px]">
     <div class="flex justify-center">
-      <img class="rounded-full" :src="`_nuxt/${img}.webp`" alt="" />
+      <img
+        class="rounded-full"
+        :src="`_nuxt/assets/avatars/arnet.webp`"
+        alt=""
+      />
     </div>
     <div class="flex flex-col justify-center md:justify-start">
       <h3 class="text-lg font-bold xl:text-xl md:text-center">
